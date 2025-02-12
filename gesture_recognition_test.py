f5ce4184fd587998856ebe1976683546bd3448c2
thesuperRL <rli@eastsideprep.org>
2025-02-12 12:22:28 -0800

    at least this works

--- gesture_recognition_test.py
+++ gesture_recognition_test.py
@@ -1,24 +1,34 @@
 import mediapipe as mp
 from mediapipe.tasks import python
-from mediapipe.tasks.python import vision, BaseOptions
+from mediapipe.tasks.python import vision
 import cv2
 import numpy as np
+from mediapipe.framework.formats import landmark_pb2
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-model_path = 'Google-Gesture-Recognition/gesture_recognizer.task'
-base_options = BaseOptions(model_asset_path=model_path)
+model_path = '/home/ryanli/PycharmProjects/UAV-Gesture-Recognization/Google-Gesture-Recognition/gesture_recognizer.task'
+
+# STEP 2: Create an GestureRecognizer object.
+base_options = python.BaseOptions(model_asset_path=model_path)
+VisionRunningMode = mp.tasks.vision.RunningMode
+GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
+GestureRecognizer = mp.tasks.vision.GestureRecognizer
+
+# Create a gesture recognizer instance with the live stream mode:
+def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
+    print('gesture recognition result: {}'.format(result))
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 252)
 cap.set(4, 288)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 
-with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
+options = vision.GestureRecognizerOptions(
+    base_options=base_options)
+with GestureRecognizer.create_from_options(options) as recognizer:
   while cap.isOpened():
     success, image = cap.read()
     if not success:
@@ -30,19 +40,31 @@ with mp_hands.Hands(
     # pass by reference.
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+    recognition_result = recognizer.recognize(mp_image)
+
+    try:
+        top_gesture = recognition_result.gestures[0][0]
+        hand_landmarks = recognition_result.hand_landmarks
+    except:
+        print("Unrecognized Gestures")
+        cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+        continue
+
+    print(recognition_result.hand_landmarks[0])
 
     # Draw the hand annotations on the image.
     image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
+    hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    hand_landmarks_proto.landmark.extend([
+        landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks[0]
+    ])
+    mp_drawing.draw_landmarks(
+        image,
+        hand_landmarks_proto,
+        mp_hands.HAND_CONNECTIONS,
+        mp_drawing_styles.get_default_hand_landmarks_style(),
+        mp_drawing_styles.get_default_hand_connections_style())
     # Flip the image horizontally for a selfie-view display.
     cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
     if cv2.waitKey(5) & 0xFF == 27:
